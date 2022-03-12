d99adfbf37f42e11f21d1d476a96389c50478e7d
chantelle mckenzie <mrspasceri@hotmail.com>
2022-03-12 08:58:26 -0800

    removed folder that was not needed

--- middleware/cars.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const rateLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 10,
-  message: 'Bonk 🔨',
-});
-
-module.exports = rateLimiter;
